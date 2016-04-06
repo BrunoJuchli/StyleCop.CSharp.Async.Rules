a2ff22b51bef53c5d62adb391ce6eb99f77ae4a4
Bruno Juchli <bruno.juchli@innocreative.ch>
2016-04-06 23:40:22 +0200

    Specifications: Added missing reference added tryout project where the latest nuget package can be downloaded and tried out (for pre-release tests)

---
+++ ReleaseTryout/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace ReleaseTryout
+{
+    // should cause some violations
+    public class Class1
+    {
+        public static async void Foo()
+        {
+            await Task.Delay(0);
+        }
+
+        public static void FooAsync()
+        {
+        }
+    }
+}
