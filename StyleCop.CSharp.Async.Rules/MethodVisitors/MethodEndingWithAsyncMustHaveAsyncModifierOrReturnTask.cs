f4d09811d18b62fc1a30c3354f5f5d7200ad17e4
Bruno Juchli <bruno.juchli@innocreative.ch>
2016-04-06 23:28:45 +0200

    added missing file (seems to be ignored, don't know why yet..)

---
+++ StyleCop.CSharp.Async.Rules/MethodVisitors/MethodEndingWithAsyncMustHaveAsyncModifierOrReturnTask.cs
@@ -0,0 +1,44 @@
+﻿namespace StyleCop.CSharp.MethodVisitors
+{
+    using System.Collections.Generic;
+
+    internal class MethodEndingWithAsyncMustHaveAsyncModifierOrReturnTask : IMethodVisitor
+    {
+        public IEnumerable<MethodViolationData> Visit(Method method)
+        {
+            if (method.HasAsyncSuffix())
+            {
+                if (IsPartOfInterface(method))
+                {
+                    if (!ReturnsTask(method))
+                    {
+                        yield return new MethodViolationData();
+                    }
+                }
+                else
+                {
+                    if (!method.IsAsyncMethod() && !ReturnsTask(method))
+                    {
+                        yield return new MethodViolationData();
+                    }
+                }
+            }
+        }
+
+        private static bool ReturnsTask(Method method)
+        {
+            return method.ReturnType.Text.RemoveGenericParantheses() == "Task";
+        }
+
+        private static bool IsPartOfInterface(Method method)
+        {
+            ICodePart current = method.Parent;
+            while (!(current is ClassBase))
+            {
+                current = current.Parent;
+            }
+
+            return current is Interface;
+        }
+    }
+}
